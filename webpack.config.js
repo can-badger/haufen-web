ea40728f0bbc1766681d670be179801492b69636
CanKarpatTD <can@thebadger.house>
2025-02-08 19:47:43 +0300

    bug fix

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+            plugins: [
+              "@babel/plugin-transform-class-properties",
+              "@babel/plugin-transform-private-methods"
+            ]
+          }
+        }
+      }
+    ]
+  }
+};
